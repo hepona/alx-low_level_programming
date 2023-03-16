f3d9366d9a732f564acd1137f9e09f103f1a5a6a
hepona <hepona10@gmail.com>
2023-03-16 13:36:32 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void){
-	char txt[]="Programming is like building a multilingual puzzle";
+	char txt[]="\"Programming is like building a multilingual puzzle";
 	puts(txt);
 
 	return 0;

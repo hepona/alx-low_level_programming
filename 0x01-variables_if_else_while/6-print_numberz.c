2394ff26bba52eb4df96f7e20a9244b3b581cf49
hepona <hepona10@gmail.com>
2023-03-17 16:57:22 -0700

    numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - all digit
  * Return: 0
@@ -7,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 48 ; i < 58 ; i++)
 	{
 		putchar(i);
 	}

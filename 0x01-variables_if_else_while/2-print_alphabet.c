01f6f5edcf8afdaaba2bbe06ef0f3c33bafe8bc8
hepona <hepona10@gmail.com>
2023-03-17 05:42:03 -0700

    wA333

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all letters
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (char i = 'a'; i <= 'z' ; i++)
+	{
+	putchar(i);
+	}
+	putchar("\n");
+
+
+	return (0);
+}

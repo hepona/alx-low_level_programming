024bb68aec0dc3ac5bbc9e9d5601378fc6087906
hepona <hepona10@gmail.com>
2023-03-17 02:50:08 -0700

    aaa

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,10 +16,10 @@ int main(void)
 	{ printf("%d is positive", n);
 	}
 	else if (n < 0)
-	{ printf("%d is zero", n);
+	{ printf("%d is negative", n);
 	}
 	else
-	{ printf("%d is negative", n);
+	{ printf("%d is zero", n);
 	}
 	return (0);
 }

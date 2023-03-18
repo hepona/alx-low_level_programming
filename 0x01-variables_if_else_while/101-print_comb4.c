74ceec33fb434c2285a63081e51c878e2bd0a147
hepona <hepona10@gmail.com>
2023-03-18 08:20:11 -0700

    advanced task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - all numbers 
+ * Return: 0
+ */
+int main(0)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		for (j = i+1 ; j < 58 ; j++)
+		{
+			for (k = j+1 ; k < 58 ; k++)
+			{
+				if (i != 58 && i != j && j != k )
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (k != 57)
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+

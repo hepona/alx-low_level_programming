684762fe2f1c2be476da87e7376a7a5c26aab901
hepona <hepona10@gmail.com>
2023-03-17 17:21:33 -0700

    numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - all combination of single digit num
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i);
+		putchar(', ');
+	}
+	putchar('\n');
+
+	return (0);
+}

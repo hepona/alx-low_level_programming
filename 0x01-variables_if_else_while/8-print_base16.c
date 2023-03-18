3c90d2e59cb6be8c61f32fa0f6cbb8bbd1fc0eea
hepona <hepona10@gmail.com>
2023-03-17 17:09:48 -0700

    numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - num and alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+	char a;
+
+	for (num = 48 ; num < 58 ; num++)
+	{
+		putchar(num);
+	}
+	for (a = 'a' ; a <= f ; a++)
+	{
+		putchar(a);
+	}
+
+	return (0);
+}

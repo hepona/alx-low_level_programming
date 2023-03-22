d62f19731c9796cd0d06711b075f82c302a7e674
hepona <hepona10@gmail.com>
2023-03-22 12:30:56 -0700

    aaa

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add -> addition of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of a+b
+ */
+int add(int a, int b)
+{
+	int r = a + b;
+
+	return (r);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * times_table -> 9 time table
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		_putchar(i

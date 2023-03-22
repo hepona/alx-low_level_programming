3a89c1909c78ef1eda99c65127a9ad31e7984f66
hepona <hepona10@gmail.com>
2023-03-22 16:06:21 -0700

    AAAAaaA

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,13 +15,17 @@ void jack_bauer(void)
 
 		for (j = 0 ; j < 10 ; j++)
 		{
-			if ((i <= 1 && j <= 9) || (j <= 2 && j <= 3))
+			if ((i <= 1 && j <= 9) || (j <= 3 && j <= 3))
 			{
 			for (k = 0 ; k < 6 ; k++)
 			{
 				for (l = 0; l < 10 ; l++)
 				{
-					printf("%d%d:%d%d", i, j, k, l);
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(58);
+					_putchar(k + '0');
+					_putchar(l + '0');
 					_putchar('\n');
 				}
 			}

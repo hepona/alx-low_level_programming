23dd3af0053918b299649f368272809c6fa4fa12
hepona <hepona10@gmail.com>
2023-03-21 19:23:27 -0700

    aaa

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit -> last digit of a num
+ * @c: integer
+ * Return: r
+ */
+int print_last_digit(int c)
+{
+	int r;
+
+	r = c % 10;
+	return (r);
+}

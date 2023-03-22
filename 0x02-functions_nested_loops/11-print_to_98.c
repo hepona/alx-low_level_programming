37fbc9d1a72c4aa31053e2494a95ae349462426a
hepona <hepona10@gmail.com>
2023-03-22 12:34:41 -0700

    aaa

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main"
+/**
+ * print_to_98 -> print to 98
+ * @n: integer
+ */
+void print_to_98(int n)
+{
+	int max = 98;
+
+	for (n = 0 ; n <= max ; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}

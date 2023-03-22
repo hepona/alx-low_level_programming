1d3f4be713c0af6299d7555039b33f5a8978445d
hepona <hepona10@gmail.com>
2023-03-22 13:15:05 -0700

    aaa

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 -> print to 98
  * @n: integer
@@ -9,7 +10,7 @@ void print_to_98(int n)
 
 	for (; n <= max ; n++)
 	{
-		_putchar(n + '0');
+		printf("%d", n);
 		_putchar(' ');
 		_putchar(',');
 	}

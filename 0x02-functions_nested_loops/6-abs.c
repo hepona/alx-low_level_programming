44a8b8dfcc08da66aadf947ca47db726fb362d4a
hepona <hepona10@gmail.com>
2023-03-21 19:06:09 -0700

    aaa

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+ * _abs -> abosulute value of an integer
+ * @int: type
+ * Return: 0
+ */
+int _abs(int)
+{
+	abs(int);
+	return (0);
+}

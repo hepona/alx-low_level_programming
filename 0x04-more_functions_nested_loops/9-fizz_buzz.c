77302da9244e58063768f52e8228c2b8f39a926a
hepona <hepona10@gmail.com>
2023-03-23 16:10:15 -0700

    FizzBuzz !

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,16 +15,16 @@ int main(void)
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
+			printf("Buzz");
 			if (i != 100)
 			{
 				printf(" ");
 			}
+
 		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
 		else
 		{
 			printf("%d ", i);

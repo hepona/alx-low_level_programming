8dbbc20074272300e167ccbde8bf615b3d23a312
hepona <hepona10@gmail.com>
2023-03-23 14:39:17 -0700

    squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,13 +8,19 @@ void print_square(int size)
 	int i;
 	int j;
 
-	for (i = 0 ; i < size ; i++)
+	if (size <= 0)
 	{
-		for (j = 0 ; j < size ; j++)
-		{
-			_putchar(35);
-		}
 		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			for (j = 0 ; j < size ; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
 }

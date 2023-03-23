e03e81a4a75f7c41304900085e3355105002a6d1
hepona <hepona10@gmail.com>
2023-03-23 09:05:55 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul -> multiply a and b
+ * @a: an integer
+ * @b: an integer
+ * Return: the result r
+ */
+int mul(int a, int b)
+{
+	int r = a * b;
+
+	return (r);
+}

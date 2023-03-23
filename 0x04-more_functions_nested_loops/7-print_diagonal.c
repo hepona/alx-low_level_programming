7c1ce88acec2ea8d7f28e8faada7dd550ff72c41
hepona <hepona10@gmail.com>
2023-03-23 14:05:46 -0700

    from 0 to 9 w/o 2 & 4

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal -> diagonal line
+ * @n: integer
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		for (j = 0 ; j < i ; j++)
+		{
+			if (n > 0)
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+}

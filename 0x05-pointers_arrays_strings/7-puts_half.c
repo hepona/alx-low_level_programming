5ece8550cf1238f45ba492bd6e456bf107f8f8c7
hepona <hepona10@gmail.com>
2023-03-27 16:52:55 -0700

    lies

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half -> print half of a string
+ * @str: character
+ */
+void puts_half(char *str)
+{
+	int i;
+	int l = strlen(str);
+
+	if (l%2 != 0)
+	{
+		for (i = 0 ; str[i] != (str[l] - 1) / 2 && str[i] != '\0' ; i++)
+		{
+			if (i >= l/2)
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = 0 ; str[i] != '0' ; i++)
+		{
+			if (i >= l/2)
+				_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}

f563f38b4f0a3e9d4a4a340e7747406ad63206a3
hepona <hepona10@gmail.com>
2023-03-27 15:49:28 -0700

    lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 -> print every other character of a string
+ * @str: a character
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}

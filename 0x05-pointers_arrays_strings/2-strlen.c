9d063e4f9a7c0b98fff1f0392508f471e7490d85
hepona <hepona10@gmail.com>
2023-03-26 21:40:04 -0700

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen -> return the length of a string
+ * @s: a character
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 #include <stdio.h>
+#include <string.h>
 
 #endif

3125c5f63e912680a9c97621d8655459a3c88b1d
hepona <hepona10@gmail.com>
2023-03-28 11:31:55 -0700

    final

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _atoi -> convert string to integer
+ * @s: a character
+ * Return: the integer , if not 0
+ */
+int _atoi(char *s)
+{
+	int l =strlen(s);
+	int i;
+
+	for (i = 0 ; i <= l ; i++)
+	{
+		if (isdigit(s[i]) || (s[i] == '+' || s[i] == '-'))
+		{
+			return (s[i] * 10 + '0');
+		}
+		else
+			return (0);
+	}
+}

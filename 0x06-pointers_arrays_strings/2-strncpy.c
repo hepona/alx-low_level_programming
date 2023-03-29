97a0819a97eade3cd9989755845972acbc3aa1c8
hepona <hepona10@gmail.com>
2023-03-29 09:52:36 -0700

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strncpy -> copy a string
+ * @dest: a character
+ * @src: a character
+ * @n: integer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	
+	for (i = 0; i <= n ; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}

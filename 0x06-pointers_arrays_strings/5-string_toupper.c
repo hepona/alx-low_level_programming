b047eb95e56668dd332cc6a4242520e743ebfa70
hepona <hepona10@gmail.com>
2023-03-29 16:28:07 -0700

    EVERYBODY IS SCREAMING AAA

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,14 +1,16 @@
 #include "main.h"
-
+#include <string.h>
+#include <ctype.h>
 /**
  * string_toupper -> change lowercase to uppercase
  * Return: s
  */
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
 	int i;
+	int l = strlen(s);
 
-	for (i = 0 ; i < strlen(s) ; i++)
+	for (i = 0 ; i < l ; i++)
 	{
 		if (islower(s[i]))
 		{

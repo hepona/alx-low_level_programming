8fd522061024ce35dc64da12f24e61c20542a212
hepona <hepona10@gmail.com>
2023-03-29 09:53:39 -0700

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,11 +5,12 @@
  * @dest: a character
  * @src: a character
  * @n: integer
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	
+
 	for (i = 0; i <= n ; i++)
 	{
 		dest[i] = src[i];

effeeb2983088f9b99395777a4ab7a009ddcfb32
hepona <hepona10@gmail.com>
2023-03-29 18:39:55 -0700

    Capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * cap_string -> capitalize all words
+ * @s: a character
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int l = strlen(s);
+
+	for (islower[i])
+	{
+		if (ispunct(s[i-1] || isspace(s[i-1]) || s[i-1] == '\n' || s[i-1] == '.'))
+		{
+			s[i] = s[i] -32;
+		}
+	}
+	return s;
+}

645baaf0c36221ad04c67bf02f955342dbf48876
hepona <hepona10@gmail.com>
2023-04-07 08:58:49 -0700

    counter argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,9 +3,10 @@
 /**
  * main -> print number of argument
  * @argc: integer
+ * @argv: character
  * Return: 0
  */
-int main (int argc , char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[]__attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);

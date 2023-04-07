990504b2be0b79dc0d670ceb8da0e0c48459c21a
hepona <hepona10@gmail.com>
2023-04-07 15:30:06 -0700

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,17 +9,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]);
+	int r, a, b;
 
-	int b = atoi(argv[2]);
-
-	int r;
-
-	if (argc != 3 && (!isdigit(a) || !isdigit(b)))
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	a = atoi(argv[1]);
+
+	b = atoi(argv[2]);
+
 	r = a * b;
 	printf("%d\n", r);
 	return (0);

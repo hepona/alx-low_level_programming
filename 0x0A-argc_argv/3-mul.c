e725563eb6ae407a5c16adc325fa44321948146a
hepona <hepona10@gmail.com>
2023-04-07 11:42:43 -0700

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,18 +4,23 @@
 #include <ctype.h>
 /**
  * main -> print multiplication of 2 number
- * Return:0
+ * @argc: integer
+ * @argv: character
+ * Return:0 if number are correct -1 otherwise
  */
 int main(int argc, char *argv[])
 {
 	int a = atoi(argv[1]);
+
 	int b = atoi(argv[2]);
+
 	if (argc == 3 && isdigit(*argv[1]) && isdigit(*argv[2]))
 	{
 		int r = a * b;
+
 		printf("%d\n", r);
 	}
-	else 
+	else
 	{
 		printf("Error\n");
 		return (-1);

892784c8e823f852cb7aa57e3dcbce7566e0eaf6
hepona <hepona10@gmail.com>
2023-04-11 11:41:22 -0700

    concact

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "string.h"
+/**
+ * str_concat -> concatenate two string
+ * @s1: character
+ * @s2: character
+ * Return: NULL on failure, otherwise m
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i,j;
+	int ls = strlen(s1);
+	int lss = strlen(s2);
+	char *m = malloc(sizeof(char) * (ls + lss + 1));
+
+	if (m == NULL)
+		return (0);
+	if (s1 == NULL)
+		return (NULL);
+	if (s2 == NULL)
+		return (NULL);
+	for (i = 0 ; s1[i] != '\0' ; i++)
+	{
+		m[i] = s1[i];
+	}
+	for (j = 0; s2[j] != '\0' ; j++)
+	{
+		m[ls + j] = s2[j];
+	}
+	return (m);
+}

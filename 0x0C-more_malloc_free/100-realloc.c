ed5eed90f61101ecd628daf14d955863cd163674
hepona <hepona10@gmail.com>
2023-04-13 21:52:56 -0700

    done

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _realloc -> reallocate memory
+ * @ptr: void
+ * @old_size: unsigned integer
+ * @new_isze: unsigned integer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * error -> handling errors
+ */
+void error()
+{
+	_putchar('E');
+	_putchar('r');
+	_putchar('r');
+	_putchar('o');
+	_putchar('r');
+	_putchar('\n');
+}
+/**
+ * main ->  multiplies two positive numbers
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	int num1, num2, mul, i;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	for (i = 0 ; argv[1][i] != '\0' ; i++)
+	{
+		if (!isdigit(argv[1][i]))
+		{
+			error();
+			return (98);
+		}
+	}
+	for (i = 0 ; argv[2][i] != '\0' ; i++)
+	{
+		if (!isdigit(argv[1][i]))
+		{
+			error();
+			return (98);
+		}
+	}
+
+	if (argc != 3)
+	{
+		error();
+		return (98);
+	}
+	mul = num1 * num2;
+	printf("%d\n", mul);
+	return (mul);
+}

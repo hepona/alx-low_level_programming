46253ae907a0eed6b3a0d812063c64b4ffb8e601
hepona <hepona10@gmail.com>
2023-04-13 17:10:35 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _calloc -> allocate memory for an array
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ * Return: pointer to the allocated memory, NULL otherwise
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	int *m;
+
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	m = malloc(sizeof(unsigned int) * (size * nmemb));
+	if (m == NULL)
+		return (NULL);
+	for (i = 0 ; i < nmemb * size ; i++)
+		m[i] = 0;
+	return (m);
+}

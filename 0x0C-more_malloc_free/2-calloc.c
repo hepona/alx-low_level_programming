0c47bc85c0469a2e738880a1c9cab90671cc502d
hepona <hepona10@gmail.com>
2023-04-13 18:04:07 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,16 +9,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	int *m;
+	void *m;
 
 	if (nmemb == 0)
 		return (NULL);
 	if (size == 0)
 		return (NULL);
-	m = malloc(sizeof(unsigned int) * (size * nmemb));
+	m = malloc(size * nmemb);
 	if (m == NULL)
 		return (NULL);
 	for (i = 0 ; i < (nmemb * size) ; i++)
-		m[i] = 0;
+		((char *)m + i) = 0;
 	return (m);
 }

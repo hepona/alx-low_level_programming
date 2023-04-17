88e5280318507f7fc8eecde0c980d0e0f976c2e3
hepona <hepona10@gmail.com>
2023-04-17 14:51:13 -0700

    print the puppy

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,8 +8,10 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d->name == NULL && d->age <= 0 && d->owner == NULL)
 		printf(" ");
+	else
+	{
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
@@ -22,4 +24,5 @@ void print_dog(struct dog *d)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
+	}
 }

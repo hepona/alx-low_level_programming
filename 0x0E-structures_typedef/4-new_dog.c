4d72d66d9627eeae5df46880a48489193f45263d
hepona <hepona10@gmail.com>
2023-04-17 15:34:42 -0700

    new puppy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,10 +6,12 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ * Return: doggo
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo = malloc(sizeof(dog_t));
+
 	if (doggo == NULL)
 		return (NULL);
 	doggo->name = name;

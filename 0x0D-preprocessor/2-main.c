c70160a95699fc4a4b5fc26073a972acb58dff17
hepona <hepona10@gmail.com>
2023-04-17 08:07:12 -0700

    file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main -> print the name of the file
- * @Return: 0
+ * main - print the name of the file
+ * Return: 0
  */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }

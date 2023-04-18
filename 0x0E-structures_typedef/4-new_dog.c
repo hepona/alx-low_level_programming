198d26aaf22a32a85a6b9e786f83f5c00c7b4a4f
hepona <hepona10@gmail.com>
2023-04-17 17:21:42 -0700

    new puppy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,8 +13,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
 
-	if (name == NULL || *name == '\0'
-			|| owner == NULL || *owner == '\0' || age < 0)
+	if (name == NULL || owner == NULL || age < 0)
+		return (NULL);
+	if (*name == '\0' || *owner == '\0')
 		return (NULL);
 	doggo = malloc(sizeof(dog_t));
 	if (doggo == NULL)

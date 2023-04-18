f15ca3a3097fdcfe20ab77de5c75612bed4d9d5c
hepona <hepona10@gmail.com>
2023-04-17 17:27:43 -0700

    new puppy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
 
-	if (name == NULL || *name == '\0' || *owner == '\0' || owner == NULL || age < 0)
+	if (name == NULL || *name == '\0' ||
+			*owner == '\0' || owner == NULL || age < 0)
 	{
 		return (NULL);
 	}

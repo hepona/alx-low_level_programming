61c569fe0c83a05b951cd136da168bc6af348254
hepona <hepona10@gmail.com>
2023-04-19 10:33:43 -0700

    my name is

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointer.h"
-#include <string.h>
 /**
  * print_name -> print a name
  * @name: a name
@@ -7,8 +6,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	int l = strlen(name);
-
-	if (name[l + 1] == '\0')
+	if (name != NULL || f != NULL)
 	f(name);
 }

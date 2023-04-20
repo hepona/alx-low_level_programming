ddc436384363c8daaedd681881aed2f58f07af16
hepona <hepona10@gmail.com>
2023-04-20 12:02:17 -0700

    sum them all!

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all -> sum all paramteres
+ * @n: an integer
+ * @...: others argument
+ * Return: s
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int r;
+	int s = 0;
+	va_list args;
+	va_start(args, n);
+
+	for (i = 0 ; i < n ; i++)
+	{
+		r = va_arg(args, int);
+		s = s + r; 
+	}
+	return (s);
+}

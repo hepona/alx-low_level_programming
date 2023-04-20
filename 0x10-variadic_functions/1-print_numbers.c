f8551ee4e0b07b806488d7450776f8dcb0d815cc
hepona <hepona10@gmail.com>
2023-04-20 15:44:49 -0700

    print all num

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_numbers -> print all number
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the fct
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	if (separator == NULL)
+		return;
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}

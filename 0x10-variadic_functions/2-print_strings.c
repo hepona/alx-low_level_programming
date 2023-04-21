724e95bd83783e1c245881a55d2018d50e3625f9
hepona <hepona10@gmail.com>
2023-04-20 17:17:33 -0700

    print print

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,14 +9,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list(args);
 	unsigned int i;
-	char *x;
 
 	va_start(args, n);
 
 	for (i = 0 ; i < n ; i++)
 	{
-		x = va_arg(args, char*);
-		printf("%s", x);
+		printf("%s", va_arg(args, char*));
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}

62db93aea8a2420b3e23e4db00c83b5d18febb2f
hepona <hepona10@gmail.com>
2023-04-27 18:39:08 -0700

    first !

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * before_main -> print a string before main()
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

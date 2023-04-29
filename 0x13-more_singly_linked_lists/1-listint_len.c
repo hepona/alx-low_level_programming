710bfe2e2a62ceb27c0598d6d733f5384291c4c3
hepona <hepona10@gmail.com>
2023-04-29 13:37:39 -0700

    lenght of list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len -> return the number 
+ * @h: a list
+ * Return: number of elements in a linked
+ */
+size_t listint_len(const listint_t *h)
+{
+	int l = 0;
+
+	while (h != NULL)
+	{
+		l++;
+		h = h->next;
+	}
+	return (l);
+}

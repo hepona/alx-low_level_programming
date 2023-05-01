47e9f1948f59e75545e94a30f9092691bd7424ae
hepona <hepona10@gmail.com>
2023-04-30 19:26:11 -0700

    node at n index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index -> return nth node of a list
+ * @head: list
+ * @index: index of the node
+ * Return: n of a node, NULL otherwise
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0 ; i != index ; i++)
+	{
+		head = head->next;
+	}
+	return (head);
+}

16abbe8e52ff0d9e8c902ac2d998d7d1185a9a75
hepona <hepona10@gmail.com>
2023-04-30 19:58:55 -0700

    node at n index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,11 +13,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
+	if (index > l)
+		return (NULL);
 	for (i = 0 ; i != index ; i++)
 	{
 		head = head->next;
 	}
-	if (index > l && index < 0)
-		return (NULL);
 	return (head);
 }

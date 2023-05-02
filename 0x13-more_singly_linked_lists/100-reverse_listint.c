7be421f79972d55d08e6606204095e6be7b8eb63
hepona <hepona10@gmail.com>
2023-05-02 14:34:19 -0700

    list ->tsil

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * reverse_listint -> reverse list
+ * @head: list
+ * Return: pointer to the first node of the reverced list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev, *nex;
+
+	if (*head == NULL)
+		return (NULL);
+	prev = NULL;
+	while (*head != NULL)
+	{
+		nex = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = nex;
+	}
+	*head = prev;
+	return (*head);
+}		

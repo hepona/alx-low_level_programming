1b21d1d64e48a5b9378ffc70378a20db1a6e8304
hepona <hepona10@gmail.com>
2023-05-04 18:17:01 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,8 +9,9 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int s = 0;
-	if (index >= sizeof(n * 8))
+
+	if (index >= sizeof(unsigned int) * 8)
 		return (-1);
-	s = n >> index & 1;
+	s = (n >> index) & 1;
 	return (s);
 }

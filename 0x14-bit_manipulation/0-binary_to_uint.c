64300aba4dda2d0ed79326eb78594923a8b77d23
hepona <hepona10@gmail.com>
2023-05-05 10:55:46 -0700

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,11 +11,11 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	int r = 0;
-	int l = strlen(b) - 1;
+	int l;
 
 	if (b == NULL)
 		return (0);
-
+	l = strlen(b) - 1;
 	for (i = 0 ; b[i] != '\0' ; i++)
 	{
 		if (b[i] != '1' && b[i] != '0')

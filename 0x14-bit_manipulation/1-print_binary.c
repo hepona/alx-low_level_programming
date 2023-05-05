9c4b8e18212b22dec3a46eacc1e4761103902b7b
hepona <hepona10@gmail.com>
2023-05-04 17:40:58 -0700

    binary print

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_binary -> print print binary representation of a number
+ * @n: number
+ */
+void print_binary(unsigned long int n)
+{
+	int m = 1;
+	int b = 0;
+
+	if (n == 0)
+		_putchar('0');
+	while (n > 0)
+	{
+		if ((n & 1) != 0)
+			b = b | m;
+		n = n >> 1;
+		m = m << 1;
+	}
+	while (m > 1)
+	{
+		m = m >> 1;
+		if ((b & m) != 0)
+			_putchar('1');
+		else
+			_putchar('0');
+	}		
+}

e7e83d4ba4a6e60480979442d7f355f95b45e645
hepona <hepona10@gmail.com>
2023-05-05 18:11:09 -0700

    not done

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * flip_bits -> return number of bits needed to flip to get from one nm to another
+ * @n: number
+ * @m: number
+ * Return: num of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, c = 0;
+
+	for (i = 1 ; n & m ; i++)
+	{
+		n = n << i;
+		c++;
+	}
+	return (c);
+}

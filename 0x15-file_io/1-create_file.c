cc8ef1b024d5447b062eea73786392b353ef3040
hepona <hepona10@gmail.com>
2023-05-08 12:21:08 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 	fclose(f);
-	if (chmod(filename, S_IRUSER, | S_IWUSR) != 0)
+	if (chmod(filename, S_IRUSR | S_IWUSR) != 0)
 		return (-1);
 	return (1);
 }

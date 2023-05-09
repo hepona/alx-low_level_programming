09d07f7136e41131f9954d51771788a7c633678d
hepona <hepona10@gmail.com>
2023-05-08 17:23:28 -0700

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (f == NULL)
 		return (-1);
-	while(text_content[i] != '\0')
+	while (text_content[i] != '\0')
 	{
 		fputc(text_content[i], f);
 		i++;

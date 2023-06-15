eaefd504d5a146b13f1f8c0af6b073b66f488c08
hepona <hepona10@gmail.com>
2023-06-15 12:45:50 -0700

    done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,11 +28,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	tmp = *h;
 	for (i = 0; i < idx - 1 && tmp != NULL; i++)
 		tmp = tmp->next;
-	if (*h == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
 	new->prev = tmp;
 	new->next = tmp->next;
 	if (tmp->next != NULL)

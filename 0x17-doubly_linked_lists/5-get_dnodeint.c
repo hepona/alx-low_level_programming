7e16e749ec66a055f24042fea6527c04b9428491
hepona <hepona10@gmail.com>
2023-06-15 09:47:48 -0700

    done

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,11 +10,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
 	for (i = 0; i != index ; i++)
 		head = head->next;
-	if (i == index)
-		return NULL
 	return (head);
 }

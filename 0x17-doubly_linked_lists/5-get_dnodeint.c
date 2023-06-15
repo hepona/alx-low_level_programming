53f5dd15fb40a7b9b090cb0ec4e0c2b05d388f18
hepona <hepona10@gmail.com>
2023-06-15 09:24:49 -0700

    done

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index -> return node at index
+ * @head: list
+ * @index: position
+ * Return: index of at a position or NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i != index ; i++)
+		head = head->next;
+	return (head);
+}

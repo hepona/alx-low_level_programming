694b05e21ad923865b6ed466930848ce9401a666
hepona <hepona10@gmail.com>
2023-06-15 12:30:14 -0700

    done

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index -> insert new node at index
+ * @h: list
+ * @idx: index
+ * @n: element to add
+ * Return: adress of new node, NULL otherwise
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	 unsigned int i;
+	 dlistint_t *new = malloc(sizeof(dlistint_t));
+	 dlistint_t *tmp;
+
+	 if (new == NULL)
+		 return (NULL);
+	 new->n = n;
+	 if (idx == 0)
+	 {
+		 new->next = *h;
+		 new->prev = NULL;
+		 if (*h != NULL)
+			 (*h)->prev = new;
+		 *h = new;
+		 return (new);
+	 }
+	 tmp = *h;
+	 for (i = 0; i < idx - 1 && tmp != NULL; i++)
+		 tmp = tmp->next;
+	 if (*h == NULL)
+	 {
+		 free(new);
+		 return (NULL);
+	 }
+	 new->prev = tmp;
+	 new->next = tmp->next;
+	 if (tmp->next != NULL)
+		 tmp->next->prev = new;
+	 tmp->next = new;
+	 return (new);
+}

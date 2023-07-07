a88d8352a119d8a749e8bf4067cf5633ad7f813a
hepona <hepona10@gmail.com>
2023-07-07 15:10:51 -0700

    4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,7 +14,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	for (i = 0; tmp != NULL; i++)
 	{
-		if (strcmp(key, tmp->key) == 0 )
+		if (strcmp(key, tmp->key) == 0)
 			return (tmp->value);
 		tmp = tmp->next;
 	}

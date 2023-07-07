02696292b123bbbfd6f046d63fe2511f48f78872
hepona <hepona10@gmail.com>
2023-07-07 06:15:31 -0700

    2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index -> give the index of a key
+ * @key: key
+ * @size: size of array
+ * Return: index at which the key/value pair should be stored
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	int indx;
+
+	(void) size;
+	indx = hash_djb2(key);
+	return (indx);
+}

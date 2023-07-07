6823e4c8a7361ce9ceb62ed24e512944b5785e40
hepona <hepona10@gmail.com>
2023-07-07 03:44:44 -0700

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,9 +10,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *nw = malloc(sizeof(hash_table_t));
 
 	if (nw == NULL)
-		return(NULL);
+		return (NULL);
 	nw->size = size;
 	nw->array = NULL;
 	free(nw);
-	return(nw);
+	return (nw);
 }

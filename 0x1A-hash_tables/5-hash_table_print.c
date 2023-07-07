d942ba9488343e954b4adaeb8381d340968ddcb8
hepona <hepona10@gmail.com>
2023-07-07 12:18:49 -0700

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -18,9 +18,9 @@ void hash_table_print(const hash_table_t *ht)
 	for (i = 0; i < ht->size ; i++)
 	{
 		if (ht->array[i] != NULL)
-		{
+                {
 			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-			if (i < ht->size - 1)
+			if (ht->array[i]->next != NULL)
 				printf(", ");
 		}
 	}

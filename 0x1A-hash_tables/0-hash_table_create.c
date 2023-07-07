d0adc24d0b98aac9b3955cb4d344e9d099414fb1
hepona <hepona10@gmail.com>
2023-07-07 04:01:47 -0700

    ht created

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,12 +13,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (nw == NULL)
 		return (NULL);
 	nw->size = size;
-	nw->array = malloc(sizeof(hash_table_t) * size);
+	nw->array = malloc(sizeof(hash_node_t) * size);
 	if (nw->array == NULL)
 		return (NULL);
 	while (i < size)
 	{
-		nw->array[1] = NULL;
+		nw->array[0] = NULL;
 		i++;
 	};
 	return (nw);

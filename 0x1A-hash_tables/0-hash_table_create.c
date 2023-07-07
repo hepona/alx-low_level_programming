b6179fca8f52a982aeb6998a36000bbc30fa5ff8
hepona <hepona10@gmail.com>
2023-07-07 03:43:37 -0700

    create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create -> create a hash table
+ * @size: size of array
+ * Return: pointer of nw hash table, NULL otherwise
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *nw = malloc(sizeof(hash_table_t));
+
+	if (nw == NULL)
+		return(NULL);
+	nw->size = size;
+	nw->array = NULL;
+	free(nw);
+	return(nw);
+}

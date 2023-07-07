1e81768a36e24ca2852eebc375f3decb93c569ba
hepona <hepona10@gmail.com>
2023-07-07 10:43:57 -0700

    4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get-> retrieve a value associated with key
+ * @ht: hash table
+ * @key: key
+ * Return: value or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+	int indx = key_index((const unsigned char *)key, ht->size);
+
+	for (i = 0; i < ht->size ; i++)
+	{
+		if (strcmp(key, ht->array[indx]->key) == 0)
+			return (ht->array[indx]->value);
+		ht->array[indx] = ht->array[indx]->next;
+	}
+	return (NULL);
+}

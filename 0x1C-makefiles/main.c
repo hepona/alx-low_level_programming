f70cf116948a459f444016900d71a19d88b46494
hepona <hepona10@gmail.com>
2023-10-25 20:48:46 +0100

    2

--- 0x1C-makefiles/main.c
+++ 0x1C-makefiles/main.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 
+ * Return: Always
  */
 int main(void)
 {
-	print_school();
-	return (EXIT_SUCCESS);
-}
+    print_school();
+    return (EXIT_SUCCESS);
+}/* School */

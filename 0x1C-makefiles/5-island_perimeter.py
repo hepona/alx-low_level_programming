bdef52eaff0c9efd9c07f64ce0ad9b0ac8da2058
hepona <hepona10@gmail.com>
2023-10-25 23:40:50 +0100

    5 not yet

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function that returns the perimeter of the island described in grid:
+"""
+def island_perimeter(grid):
+    p = 0
+    for i in grid:
+        for j in i:
+            if j == 1:
+                p = p + 4
+    return p

3677622d6c5d33f28b069ec1a215d5b04b450842
hepona <hepona10@gmail.com>
2024-02-08 13:49:51 +0100

    bs

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,27 +1,31 @@
 #include <search.h>
 #include <stdio.h>
 /**
-* binary_search - function that searches
-* for a value in a sorted array of integer
-* @array: pointer to the first element of the array to search in
-* @size: number of elements in array
-* @value: value to search for
-* Return: the first index where value is located, -1 otherwise
-*/
+ * binary_search - function that searches
+ * for a value in a sorted array of integer
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: the first index where value is located, -1 otherwise
+ */
 int binary_search(int *array, size_t size, int value)
 {
-size_t i;
-if (array == NULL)
-return (-1);
+    size_t midle = size / 2;
+    size_t i, j;
+    if (array == NULL)
+        return (-1);
 
-for (i = 0; i < size; i++)
-{
-printf("Searching in array:%ld\n", size - i);
-}
-for (i = 0; i < size; i++)
-{
-if (array[i] == value)
-return (i);
-}
-return (-1);
+    if (array[midle] == value)
+        return (midle);
+    for (i = midle; i > 0; i--)
+    {
+        printf("Searching in array: ");
+        for (j = 0; j < size ; j++)
+            printf("%d, ", array[j]);
+        printf("\n");
+        if (array[i] == value)
+            return (i);
+        binary_search(array + i + 1, size - i - 1, value);
+    }
+    return (-1);
 }

5aa74cfaf8d8f0483071300224a4484dfb5b157e
hepona <hepona10@gmail.com>
2024-02-08 13:52:02 +0100

    bs

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,22 +10,22 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t midle = size / 2;
-    size_t i, j;
-    if (array == NULL)
-        return (-1);
+size_t midle = size / 2;
+size_t i, j;
+if (array == NULL)
+return (-1);
 
-    if (array[midle] == value)
-        return (midle);
-    for (i = midle; i > 0; i--)
-    {
-        printf("Searching in array: ");
-        for (j = 0; j < size ; j++)
-            printf("%d, ", array[j]);
-        printf("\n");
-        if (array[i] == value)
-            return (i);
-        binary_search(array + i + 1, size - i - 1, value);
-    }
-    return (-1);
+if (array[midle] == value)
+return (midle);
+for (i = midle; i > 0; i--)
+{
+printf("Searching in array: ");
+for (j = 0; j < size; j++)
+printf("%d, ", array[j]);
+printf("\n");
+if (array[i] == value)
+return (i);
+binary_search(array + i + 1, size - i - 1, value);
+}
+return (-1);
 }
